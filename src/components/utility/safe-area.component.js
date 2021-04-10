1219aea4734283c016d6c457354e9a92463445c1
prana-magar <Prakashrana9847067614@gmail.com>
2021-04-09 22:11:00 -0400

    Added view screen update

--- src/components/utility/safe-area.component.js
+++ src/components/utility/safe-area.component.js
@@ -4,4 +4,5 @@ import styled from "styled-components/native";
 export const SafeArea = styled(SafeAreaView)`
   flex: 1;
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+  margin: ${(props) => props.theme.space[2]};
 `;
